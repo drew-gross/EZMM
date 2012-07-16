e8cdf159211de1fa27480469e2c838f48d32bcd0
Drew Gross <drew.a.gross@gmail.com>
2012-07-16 01:30:21 -0400

    added removeNoise but it doesn't work very well

--- findBlankSections.py
+++ findBlankSections.py
@@ -28,7 +28,7 @@ def rowsWithProperty(image, property):
 def colsWithProperty(image, property):
     return [property(image[:,i]) for i in xrange(image.shape[1])]
 
-def findTrueSections(array):
+def findSectionsWithProperty(array, property):
     sections = []
     currentVal = None
     for index, val in enumerate(array):
@@ -36,32 +36,48 @@ def findTrueSections(array):
             sections.append({'val':val, 'count':1, 'startpos':index})
             currentVal = val
         else:
-            sections[-1]['count'] += 1;
+            sections[-1]['count'] += 1
+
     sectionsWithProperty = filter(itemgetter('val'), sections)
     return sectionsWithProperty
 
 def largeRowsWithProperty(image, sectionProperty):
     trueSections = rowsWithProperty(image, sectionProperty)
     minSecCount = max(trueSections, key=itemgetter('count'))['count']/10
-    return findTrueSections(rowHasProperty)
+    return findSectionsWithProperty(rowHasProperty, lambda x:x)
 
 def splitToRows(image):
     grayImage = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
-    sections = findTrueSections(rowsWithProperty(grayImage, lambda x: not isEmpty(x)))
+    rows = rowsWithProperty(grayImage, lambda x: not isEmpty(x))
+    rows = removeNoise(rows)
+    sections = findSectionsWithProperty(rows, lambda x:x)
     return map(lambda section: rowFromSection(image, section), sections)
 
 def splitToCols(image):
     grayImage = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
-    sections = findTrueSections(colsWithProperty(grayImage, lambda x: not isEmpty(x)))
+    cols = colsWithProperty(grayImage, lambda x: not isEmpty(x))
+    filteredCols = removeNoise(removeNoise(cols))
+    sections = findSectionsWithProperty(filteredCols, lambda x:x)
     return map(lambda section: colFromSection(image, section), sections)
+
+def removeNoise(array):
+    copy = [None] * len(array)
+    for i in xrange(len(array)):
+        if  (array[i] != array[i - 1]) and \
+            (array[i] != array[i + 1]):
+            copy[i] = not array[i]
+        else:
+            copy[i] = array[i]
+    return copy
 
 image = cv2.imread("""C:\Users\Drew Gross\Documents\Projects\EZMM\Test data\CHE101MS08N.jpg\page.jpg""")
 
 letters = []
 rows = splitToRows(image)
 for row in rows:
+    [vLine(row, index) for index, col in enumerate(colsWithProperty(cv2.cvtColor(row, cv2.cv.CV_BGR2GRAY), isEmpty)) if col]
     cols = splitToCols(row)
     for col in cols:
         letters.append(col)
 
-[cv2.imwrite(str(index) + '.jpg', row) for index, row in enumerate(letters)]
+[cv2.imwrite(str(index) + '.png', row) for index, row in enumerate(rows)]
